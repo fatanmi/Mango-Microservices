07be466cacb2515dd8621d6a0dd3a29df843c711
Ebenezer Fatanmi <efatanmi@loraxcompliance.com>
2024-01-24 17:56:21 +0000

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    public class CouponAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
