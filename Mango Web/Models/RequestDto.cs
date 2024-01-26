b69a9879a9d655655cc770ae04176b030609e954
Ebenezer Fatanmi <efatanmi@loraxcompliance.com>
2024-01-26 16:23:50 +0000

    Created IServices and Service

---
+++ Mango Web/Models/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using static Mango_Web.Utilities.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public Object Data { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Mango Web/Utilities/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango_Web.Utilities
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET, POST, PUT, DELETE, TRACE,
+        }
+    }
+}
